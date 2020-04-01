fdd0138705ca87741a7dbfa025179a4f36deb5ca
Juan Alcántara <j.alejandrominaya@gmail.com>
2020-03-31 18:44:46 -0600

    Update gitignore to include gradle files

---
+++ app/src/main/java/com/itesm/powermanagement/receivers/BatteryStatusReceiver.kt
@@ -0,0 +1,23 @@
+package receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+import android.widget.TextView
+
+class BatteryStatusReceiver(val targetText: TextView) : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val status = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+        if(status == BatteryManager.BATTERY_PLUGGED_AC) {
+            targetText.text = "Plugged to AC"
+        } else if (status == BatteryManager.BATTERY_PLUGGED_USB){
+            targetText.text = "Plugged to USB"
+        } else if (status == BatteryManager.BATTERY_PLUGGED_WIRELESS) {
+            targetText.text = "Plugged to Wireless Charger"
+        } else {
+            targetText.text = "On battery. Not Plugged"
+        }
+
+    }
+}
